9c465fac47919a419bfef18679b8326c168643cc
Seokmin Hong <ghdtjrald240@gmail.com>
2022-03-07 20:45:47 +0900

    Fix pushing script

--- push.sh
+++ push.sh
@@ -1,10 +1,14 @@
 # Split subtree
 SPLIT=$(git subtree split --prefix $PREFIX)
-# Switch to the subtree branch
-git switch $BRANCH
-# Merge the splitted branch
-git merge -X subtree="$PREFIX" --allow-unrelated-histories $SPLIT
+# Switch to the splitted commit
+git switch $SPLIT
 # Push to origin
 git subtree push --prefix=$PREFIX $REPO $REF
-# Switch to the original branch
+
+# Pull changes
+# Switch to the branch for pulling subtree
+git switch $BRANCH
+git subtree pull --prefix $PREFIX $REPO $REF --squash
+
 git switch $CUR_BRANCH
+git merge --squash --allow-unrelated-histories $BRANCH
