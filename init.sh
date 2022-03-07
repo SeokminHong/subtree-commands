0e382241e2fe2968738c708aff1dfe5d2852e02f
Seokmin Hong <ghdtjrald240@gmail.com>
2022-03-07 15:40:52 +0900

    Set switch to branch

--- init.sh
+++ init.sh
@@ -2,5 +2,6 @@ set -e
 
 # Add the subtree repository as a remote
 git remote add -f $REMOTE $REPO
+git switch -c $BRANCH
 git subtree add --prefix $PREFIX $REMOTE $IDENTIFIER --squash
 mkdir -p $PREFIX
--- pull.sh
+++ pull.sh
@@ -5,4 +5,3 @@ git switch -c $BRANCH
 
 git fetch $REMOTE $IDENTIFIER
 git subtree pull --prefix $PREFIX $REMOTE $IDENTIFIER --squash
-
