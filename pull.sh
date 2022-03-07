24e6cd65364d54330f44cd7ad5867742f125ee0b
Seokmin Hong <ghdtjrald240@gmail.com>
2022-03-07 19:07:42 +0900

    Implement pushing

--- pull.sh
+++ pull.sh
@@ -1,8 +1,19 @@
 set -e
 
+# Has changed files
+if [[ `git status --porcelain` ]]; then
+  git stash -u
+  DIRTY=1
+fi
+
 # Switch to the branch for pulling subtree
 git switch $BRANCH
 git subtree pull --prefix $PREFIX $REPO $REF --squash
 
 git switch $CUR_BRANCH
 git merge --squash --allow-unrelated-histories $BRANCH
+
+# Pop stashed files
+if [[ $DIRTY ]]; then
+  git stash pop
+fi
--- push.sh
+++ push.sh
@@ -1,1 +1,10 @@
+# Split subtree
+SPLIT=$(git subtree split --prefix $PREFIX)
+# Switch to the subtree branch
+git switch $BRANCH
+# Merge the splitted branch
+git merge -X subtree="$PREFIX" --allow-unrelated-histories $SPLIT
+# Push to origin
 git subtree push --prefix=$PREFIX $REPO $REF
+# Switch to the original branch
+git switch $CUR_BRANCH
